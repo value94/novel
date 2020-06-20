ec5573679d18ac70655699d1c3be4f639aef1682
value94 <x_x9403@163.com>
2020-06-20 21:44:17 +0800

    fix database config

--- config/database.php
+++ config/database.php
@@ -11,27 +11,27 @@
 
 return [
     // 数据库类型
-    'type'            => 'mysql',
+    'type'            => env('database.type', 'mysql'),
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => env('database.hostname', ''),
     // 数据库名
-    'database'        => 'xiaoshuo',
+    'database'        => env('database.database', 'snake'),
     // 用户名
-    'username'        => 'root',
+    'username'        => env('database.username', ''),
     // 密码
-    'password'        => 'root',
+    'password'        => env('database.password', ''),
     // 端口
-    'hostport'        => '3306',
+    'hostport'        => env('database.hostport', '3306'),
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
     'params'          => [],
     // 数据库编码默认采用utf8
-    'charset'         => 'utf8',
+    'charset'         => 'utf8mb4',
     // 数据库表前缀
-    'prefix'          => 'rc_',
+    'prefix'          => env('database.prefix', 's_'),
     // 数据库调试模式
-    'debug'           => false,
+    'debug'           => env('database.debug', false),
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
     'deploy'          => 0,
     // 数据库读写是否分离 主从式有效
@@ -47,11 +47,11 @@ return [
     // 数据集返回类型
     'resultset_type'  => 'array',
     // 自动写入时间戳字段
-    'auto_timestamp'  => false,
+    'auto_timestamp'  => 'datetime',
     // 时间字段取出后的默认时间格式
-    'datetime_format' => false,
+    'datetime_format' => 'Y-m-d H:i:s',
     // 是否需要进行SQL性能分析
-    'sql_explain'     => false,
+    'sql_explain'     => env('database.sql_explain', false),
     // Builder类
     'builder'         => '',
     // Query类
